0b2d2acf1b2a94fe398b03610c1e3ba5061b4476
David <ha_257@hotmail.com>
2022-08-18 13:18:01 +0200

    Unit test to validate test framework is working

---
+++ src/test/java/SuccessTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SuccessTest {
+
+    @Test
+    public void successfully_test_testing_framework() {
+        Assertions.assertTrue(true);
+        Assertions.assertFalse(false);
+    }
+
+    @Test
+    public void assertion_should_fail() {
+        Assertions.fail();
+    }
+}
