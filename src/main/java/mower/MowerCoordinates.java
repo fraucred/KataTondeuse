4e71048201f08d2febba8d1ad5640cebc32235a9
David <ha_257@hotmail.com>
2022-08-18 13:22:30 +0200

    Add MowerCoordinates and Axis classes

---
+++ src/main/java/mower/MowerCoordinates.java
@@ -0,0 +1,9 @@
+package mower;
+
+import mower.coordinates.Axis;
+
+public class MowerCoordinates {
+    public Object getAxis() {
+        return new Axis();
+    }
+}
---
+++ src/main/java/mower/coordinates/Axis.java
@@ -0,0 +1,4 @@
+package mower.coordinates;
+
+public class Axis {
+}
