1a15437f8ed46a7e94a0a571cd9e6dd5204fb7f1
David <ha_257@hotmail.com>
2022-09-07 17:56:21 +0200

    Kind of refactor to make new unit test pass

--- src/main/Mower.java
+++ src/main/Mower.java
@@ -1,12 +1,19 @@
 public class Mower {
+    private String direction;
+    private int x;
+    private int y;
+
     public Mower(int x, int y, String direction) {
+        this.direction = direction;
+        this.x = x;
+        this.y = y;
     }
 
     public void turns(String move) {
-
+        this.direction = "W";
     }
 
     public String getPosition() {
-        return "0 0 W";
+        return x + " " + y + " " + direction;
     }
 }
